82171ef3a67bd0b25b1be542f1989773c8911f23
Baran Korkmaz <korkmazbarann@gmail.com>
2022-12-16 17:52:06 +0300

    Basics of Hibernate
    Configuration of hibernate,Annotations;Entity.Table,Id,SequenceGenerator,Column, Transient,Lob.Temporal

---
+++ Hibernate/basics of hibernate/PojoApp.java
@@ -0,0 +1,27 @@
+package spring.deneme;
+
+import domain.Pojo;
+import hibernate.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class PojoApp {
+	public static void main(String[] args) {
+		Date date = new Date();
+		Pojo pojo = new Pojo();
+		pojo.setName("Baran");
+		pojo.setValue(new BigDecimal("5.12"));
+		pojo.setDate(date);
+		pojo.setTime(date);
+		pojo.setDateAndTime(date);
+		pojo.setTemporary("temporary");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = session.beginTransaction();
+		session.merge(pojo);
+		transaction.commit();
+	}
+
+}
